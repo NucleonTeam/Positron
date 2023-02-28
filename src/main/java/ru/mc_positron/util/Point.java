7b7cdf51512a2da3de578bbb73ee9b6437d39def
qPexLegendary <terentev.andrey.2002@gmail.com>
2023-02-28 18:56:06 +0700

    added Point class

---
+++ src/main/java/ru/mc_positron/util/Point.java
@@ -0,0 +1,37 @@
+package ru.mc_positron.util;
+
+import lombok.NonNull;
+import org.spongepowered.math.vector.Vector3d;
+
+public final class Point {
+
+    private final Vector3d position;
+    private final double yaw, pitch, headYaw;
+
+    private Point(@NonNull Vector3d position, double yaw, double pitch, double headYaw) {
+        this.position = position;
+        this.yaw = yaw;
+        this.pitch = pitch;
+        this.headYaw = headYaw;
+    }
+
+    public @NonNull Vector3d getPosition() {
+        return position;
+    }
+
+    public double getYaw() {
+        return yaw;
+    }
+
+    public double getHeadYaw() {
+        return headYaw;
+    }
+
+    public double getPitch() {
+        return pitch;
+    }
+
+    public static @NonNull Point of(@NonNull Vector3d position, double yaw, double pitch, double headYaw) {
+        return new Point(position, yaw, pitch, headYaw);
+    }
+}
